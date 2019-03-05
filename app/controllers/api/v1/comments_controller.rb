f230590c17bf060223b995b05930d2813e6e1842
Anik Islam <Anik1606@gmail.com>
2019-03-05 01:30:30 -0500

    full CRUD

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -15,11 +15,26 @@ class Api::V1::CommentsController < ApplicationController
     render json: @comment
   end
 
+  def update
+
+    @comment= Comment.find(params[:id])
+    @comment.content = params[:content]
+    @comment.save
+    render json: @comment
+  end
+
+  def destroy
+    @comment= Comment.find(params[:id])
+    @comment.destroy
+    @comments = Item.find(params[:item_id]).comments
+    render json: @comments
+  end
+
 
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :item_id);
+    params.require(:comment).permit(:content, :item_id, :id);
   end
 end
